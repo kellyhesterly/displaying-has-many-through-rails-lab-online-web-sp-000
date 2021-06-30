5a0e24178949e0d4b1277454610d8ac6044766b4
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-30 22:16:06 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,38 +1,7 @@
 class AppointmentsController < ApplicationController
-  def index
-    @appointments = Appointment.all
-  end
 
   def show
     @appointment = Appointment.find(params[:id])
   end
-
-  def new
-    @appointment = Appointment.new
-  end
-
-  def create
-    @appointment = Appointment.new(appointment_params)
-
-    if @appointment.save
-      redirect_to @appointment
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-
-  def appointment_params
-    params.require(:appointment).permit(:name, :department)
-  end
+  
 end
