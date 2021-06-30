0ff1b8c3b994730285102b12f1af742371e0b4e9
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-30 22:31:10 +0000

    Automatically backed up by Learn

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,8 +1,4 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
-
-  def appointment_datetime
-  end
-   @appointment.appointment_datetime.strftime("%B %d, %Y at %k:%M")
 end
