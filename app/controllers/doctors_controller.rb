01cb3ade0058a4a80106825c9812c9f9bde7672c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-30 22:33:06 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -6,19 +6,4 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:id])
   end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
