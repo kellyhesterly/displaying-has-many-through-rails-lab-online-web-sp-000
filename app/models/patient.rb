262b88bcf5561e0aa6c48568768c66646f988d5c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-30 22:56:52 +0000

    Automatically backed up by Learn

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,4 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
-
-  def appointment_count
-    self.appointment.count
-    binding.pry
-  end
 end
